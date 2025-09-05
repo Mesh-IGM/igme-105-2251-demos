6620618bfa67666509a018f584697e9771c060ed
Erika Mesh <esmvcs@rit.edu>
2025-09-05 11:46:06 -0400

    Fri 9/5 - data types & vars

---
+++ Demos/DataTypes_Vars/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DataTypes_Vars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare AND initialize together
+            const int DaysInWeek = 7;
+            int myAge = 5;
+            // NO int MyAge = 5;
+            string address = "1 Lomb Memorial";
+            double accountBalance = 12.34;
+
+            // declare only
+            int num;
+
+            Console.WriteLine("Shiro" + myAge);
+
+            // init later - just has to be done before I use it
+            num = myAge % 5;
+
+            // all the same
+            num = num + 1;
+            num += 1;
+            num++;
+
+            Console.WriteLine(num);
+            Console.WriteLine(DaysInWeek);
+
+        }
+    }
+}
