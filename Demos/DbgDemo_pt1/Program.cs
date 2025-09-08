6047571e71c8bb4cdd580907a3a7cd94c6414c80
Erika Mesh <esmvcs@rit.edu>
2025-09-08 09:26:42 -0400

    Dbg demo -- circle collisions

---
+++ Demos/DbgDemo_pt1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DbgDemo_pt1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Data, variables, math expressions, string concatenation, assignments
+
+            // ***** collisions *******
+
+            // center 1
+            int xA = 0;
+            int yA = 0;
+
+            // center 2
+            int xB = 4;
+            int yB = 5;
+
+            // radii
+            int rA = 1;
+            int rB = 2;
+
+            // https://www.mathsisfun.com/algebra/distance-2-points.html
+            int distSquared = (xA - xB) * (xA - xB) + (yA - yB) * (yA - yB);
+            int sumRadii = rA + rB;
+
+            Console.WriteLine("Distance squared: "+distSquared);
+            Console.WriteLine("Sum radii - squared: " + sumRadii*sumRadii);
+            
+
+
+            // gravitational math
+
+        }
+    }
+}
