da793211d488d58c7a3cb86a46413907a3386d94
Erika Mesh <esmvcs@rit.edu>
2025-09-19 09:44:18 -0400

    Fri 9/19 - Input & Strings demo

---
+++ Demos/InputStrings/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace InputStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "???";
+            string allCaps;
+
+            Console.Write("Enter your name: ");
+            //name = Console.ReadLine();
+            //name = name.Trim();
+            name = Console.ReadLine().Trim();
+
+            Console.WriteLine("Hi "+name+"!\nYour name has "+name.Length+"!");
+
+            Console.WriteLine("String with escape chars\n\t\"skjhskjfghdksfgh\\dkjfghdkjfhgdkjghf\"");
+
+            name.ToUpper();
+            Console.WriteLine(name);
+
+            allCaps = name.ToUpper();
+            Console.WriteLine(allCaps);
+
+            Console.WriteLine("The first 3 letters are: " + allCaps.Substring(0, 3));
+        }
+    }
+}
