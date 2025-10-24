57bedd3be323337171ab4239342878855af77693
Erika Mesh <esmvcs@rit.edu>
2025-10-24 10:10:06 -0400

    Fri 10/24 - Random & intro to OO

---
+++ Demos/Random_OO/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Random_OO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Only want ONE instance of random per project
+            Random rng = new Random();
+            int[] randomNums = new int[100];
+            for (int i = 0; i<randomNums.Length;i++)
+            {
+                // BAD Random rng = new Random();
+                //Console.WriteLine(rng.Next(20));
+                randomNums[i] = rng.Next(randomNums.Length); // [0, len-1)
+            }
+        }
+    }
+}
