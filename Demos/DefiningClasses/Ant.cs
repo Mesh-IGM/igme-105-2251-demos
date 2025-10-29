8b5c201ed574569911723bf380c6954eb18b1e1c
Erika Mesh <esmvcs@rit.edu>
2025-10-29 10:07:45 -0400

    Wed 10/29 - Defining Classes

---
+++ Demos/DefiningClasses/Ant.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    internal class Ant
+    {
+        // Fields
+        private string color;
+        private string name;
+        private int limbCount;
+        private double biteStr; // percent of max
+        private double biteLikelihood;
+        private bool isQueen;
+        private int currentLoad;
+        private const int WeightMax = 100;
+
+        // Properties -- coming soon
+
+        // Default Constructor
+        /*
+        public Ant()
+        {
+            color = "black";
+            name = "Worker";
+            limbCount = 6;
+            biteStr = 1.0;
+            biteLikelihood = 0.0;
+            isQueen = false;
+        }
+        */
+
+        // Param Constructor
+        public Ant(/*string nameParam,*/ string name, string color)
+        {
+            //name = nameParam;
+            this.name = name;
+            this.color = color;
+            limbCount = 6;
+            biteStr = 1.0;
+            biteLikelihood = 0.0;
+            isQueen = false;
+        }
+
+        // Methods
+
+        public void GetFood(int amount)
+        {
+            currentLoad += amount;
+            currentLoad = Math.Min(currentLoad, WeightMax);
+            Console.WriteLine($"{name} - {currentLoad}");
+        }
+
+
+    }
+}
---
+++ Demos/DefiningClasses/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DefiningClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ant defaultAnt = new Ant();
+            Ant shiro = new Ant("Shiro", "black");
+            Ant lacy = new Ant("Lacy", "red");
+            Random rng = new Random();
+
+            for(int i = 0; i < 10; i++)
+            {
+                shiro.GetFood(rng.Next(5,20));
+                lacy.GetFood(rng.Next(5, 20));
+            }
+        }
+
+        public static void DoStuff()
+        {
+
+        }
+    }
+
+}
