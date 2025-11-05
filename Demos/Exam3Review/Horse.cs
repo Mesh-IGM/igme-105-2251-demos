6dd6814e1de8adbb033a49b89e1c645eef559a9e
Erika Mesh <esmvcs@rit.edu>
2025-11-05 10:35:55 -0500

    Wed 11/5 - exam 3 review

---
+++ Demos/Exam3Review/Horse.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam3Review
+{
+    /// <summary>
+    /// Store data about a horse
+    /// </summary>
+    internal class Horse
+    {
+        // define the class members
+
+        // fields
+        private string color;
+        private double speed;
+        private const int HorsePower = 5; // 5m/sec
+
+        // constructors
+
+        public Horse()
+        {
+            color = "horse";
+            speed = 1;
+        }
+
+        public Horse(string colorStr, double speed)
+        {
+            this.color = colorStr;
+            color = colorStr;
+            this.speed = speed;
+        }
+
+        // methods
+        public void Run(int meters)
+        {
+            Console.WriteLine("The {0} horse went {1}m in {2} seconds.",
+                color,
+                meters,
+                meters * HorsePower * speed);
+        }
+
+        public void Flee()
+        {
+            while(true)
+            {
+                this.Run(100);
+            }
+        }
+
+        public static void PrintFacts()
+        {
+            Console.WriteLine("...facts...");
+        }
+
+    }
+}
---
+++ Demos/Exam3Review/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Exam3Review
+{
+    /*
+     * how does the "this" keyword work again?
+     * Can you go over how to use TryParse?
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* - not finished by I know I need to ....
+            Math.Pow()
+            */
+
+            /*
+            int age;
+            bool readAge = false;
+
+            while (!readAge)
+            {
+                try
+                {
+                    Console.Write("How old are you? ");
+                    age = int.Parse(Console.ReadLine());
+                    readAge = true;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.WriteLine("\n\n\n");
+            readAge = false;
+            age = -1;
+            while (!readAge)
+            {
+                Console.Write("How old are you? ");
+                readAge = int.TryParse(Console.ReadLine(), out age);
+                if(!readAge)
+                { 
+                    Console.WriteLine("Invalid number"); 
+                }
+            }
+            */
+
+            string name = "Shiro";
+            string other = new string("lacy");
+            int[] nums = new int[3] { 1, 2, 3 };
+            int[] nums2 = { 1, 2, 3 };
+
+            char firstLetter = name[0]; // ok
+            // name[0] = 'a'; // not okay
+            char letter = 'b';
+            letter = (char)(((int)letter - (int)'a') + (int)'A');
+
+            Horse aiden = new Horse("palamino",1);
+            Horse cosmo = new Horse("grey",1.5);
+
+            //Horse.Run(10);
+            Horse.PrintFacts();
+            aiden.Run(10);
+            cosmo.Run(10);
+
+           // aiden.Flee();
+
+           // Console.WriteLine(aiden.color);
+        }
+    }
+}
